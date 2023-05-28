69c45c7aa801a454760e89f495d86f903128c637
Bellouard <lyn-n@live.fr>
2023-05-28 14:13:59 +0200

    publish npm

---
+++ dist/components/Modale.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+require("./style/style");
+function Modale(props) {
+  const text = props.text || 'Employee Created !';
+  if (props.display) {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "modale"
+    }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
+      onClick: props.setDisplayModale
+    }, "X"), /*#__PURE__*/React.createElement("p", null, text)));
+  }
+  return /*#__PURE__*/React.createElement("div", null);
+}
+var _default = Modale;
+exports.default = _default;
