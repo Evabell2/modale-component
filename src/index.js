b89372b986e5b3baf03efd8a36ede584d248425b
Bellouard <lyn-n@live.fr>
2023-05-28 15:17:44 +0200

    add index.js in src

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Modale } from './lib/components/Modale';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Modale />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
